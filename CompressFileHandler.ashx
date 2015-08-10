37a9565cad18a4fba6be2f44782402bc48a2868c
maharishi.bhatia@gmail.com <maharishi.bhatia@gmail.com>
2015-08-10 00:57:17 -0400

    zip service funtionality tie up.

---
+++ CompressFileHandler.ashx
@@ -0,0 +1,27 @@
+﻿<%@ WebHandler Language="C#" Class="CompressFileHandler" %>
+
+using System;
+using System.Web;
+
+public class CompressFileHandler : IHttpHandler
+{
+
+    public void ProcessRequest(HttpContext context)
+    {
+
+        var oSR = new System.IO.StreamReader(context.Request.InputStream);
+        var zip = Newtonsoft.Json.JsonConvert.DeserializeObject<ZipCruncher>(oSR.ReadToEnd());
+        zip.Zip();
+        context.Response.ContentType = "text/json";
+        context.Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(zip));
+    }
+
+    public bool IsReusable
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+}
--- Content/js/app.js
+++ Content/js/app.js
@@ -1,5 +1,5 @@
 ﻿var controllers = {};
-var app = angular.module("maharishiApp", ['ngRoute',]);
+var app = angular.module("maharishiApp", ['ngRoute']);
 app.controller(controllers);
 app.config(function ($routeProvider) {
     $routeProvider
@@ -23,6 +23,33 @@ controllers.navCtrl = function ($scope, $location) {
     }
 }
 
-controllers.homeCtrl = function ($scope, $location) {
-
+controllers.homeCtrl = function ($scope, $location, zipFactory) {
+    $scope.zipFile = function () {
+        zipFactory.zipFile({ url: $scope.url, filename: $scope.filename }, function (result) {
+            $scope.downloadlabel = "Downloaded File: ";
+            $scope.filelink = result.filelink;
+            $scope.downloadfilename = result.filename;
+        });
+    }
 }
+
+app.factory('zipFactory', function ($http) {
+    var factory = {};
+    factory.zipFile = function (zipRequestData, callback) {
+        var svc = $http.post('/CompressFileHandler.ashx', { url: zipRequestData.url, filename: zipRequestData.filename }).
+                      success(function(data, status, headers, config) {
+                          // this callback will be called asynchronously
+                          // when the response is available
+                          //console.log(status);
+                          //console.log(data);
+                          callback(data);
+                      }).
+                      error(function (data, status, headers, config) {
+                          // called asynchronously if an error occurs
+                          // or server returns response with an error status.
+                          console.log(status);
+                      });
+    }
+
+    return factory;
+});
